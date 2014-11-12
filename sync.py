0cafd43016785e86897eadce360811e7716c888b
Roman Dvorak <roman-dvorak@email.cz>
2014-11-12 19:51:52 +0100

    Update sync.py

--- sync.py
+++ sync.py
@@ -46,10 +46,10 @@ def UploadTo(location):
 	#print "-------"+"rsync -vvarz --exclude='*/*'" + path_sort + path_data  + " " + location + config.UserSpace + "/" +   config.StationSpace + "/"+config.path_data
 	#print "-------"+"rsync -vvarz --exclude='*/*'" + path_sort + " " + location + config.UserSpace + "/" + config.StationSpace + "/"
 	#print "-------"+"timeout 1000 rsync -vvarz --exclude='*/*'" + path_sort + path_audio + " " + location + "/" + config.UserSpace + "/" + config.StationSpace + "/"+config.path_audio
-	os.system("rsync -vvarz --exclude='*/*'" + path_sort + path_image + " " + location + config.UserSpace + "/" +   config.StationSpace + "/"+config.path_image)
-	os.system("rsync -vvarz --exclude='*/*'" + path_sort + path_data  + " " + location + config.UserSpace + "/" +   config.StationSpace + "/"+config.path_data)
-	os.system("rsync -vvarz --exclude='*/*'" + path_sort + " " + location + config.UserSpace + "/" + config.StationSpace + "/")
-	os.system("timeout 1000 rsync -vvarz --exclude='*/*'" + path_sort + path_audio + " " + location + "/" + config.UserSpace + "/" + config.StationSpace + "/"+config.path_audio) # 1700s = 28,3333min
+	os.system("rsync -varz --exclude='*/*'" + path_sort + path_image + " " + location + config.UserSpace + "/" +   config.StationSpace + "/"+config.path_image)
+	os.system("rsync -varz --exclude='*/*'" + path_sort + path_data  + " " + location + config.UserSpace + "/" +   config.StationSpace + "/"+config.path_data)
+	os.system("rsync -varz --exclude='*/*'" + path_sort + " " + location + config.UserSpace + "/" + config.StationSpace + "/")
+	os.system("timeout 1000 rsync -varz --exclude='*/*'" + path_sort + path_audio + " " + location + "/" + config.UserSpace + "/" + config.StationSpace + "/"+config.path_audio) # 1700s = 28,3333min
 	f.write('SYNC.PY\t\t|| ' + strftime("%d %b %Y %H:%M:%S", gmtime()) + '\t >>    Finish of upload \n')
 	f.close()
 	print "SYNC.PY \t|| ", strftime("%d %b %Y %H:%M:%S", gmtime()), " Synchronisation was finished!"
