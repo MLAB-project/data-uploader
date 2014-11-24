4908fc374f736d30c16e6a48b2b831b734bbed51
Jan Milík <milikjan@fit.cvut.cz>
2014-11-24 19:39:24 +0100

    Start a new branch with the new uploader.

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+from setuptools import setup, find_packages
+import sys
+import os
+import os.path as path
+
+
+os.chdir(path.realpath(path.dirname(__file__)))
+sys.path.insert(1, 'src')
+import rmds_data_uploader
+
+
+setup(
+    name             = 'rmds_data_uploader',
+    version          = rmds_data_uploader.__version__,
+    author           = 'Jan Milík',
+    author_email     = 'milikjan@fit.cvut.cz',
+    description      = 'Common tools and functions for MLAB python modules.',
+    long_description = rmds_data_uploader.__doc__,
+    url              = 'https://github.com/bolidozor/RMDS-data-uploader',
+
+    #packages    = ['pymlab', 'pymlab.sensors', 'pymlab.tests', ],
+    packages    = find_packages("src"),
+    package_dir = {'': 'src'},
+    provides    = ['rmds_data_uploader'],
+    install_requires = [ 'mlabutils', 'python-daemon', 'lockfile', ],
+    keywords = ['MLAB', 'bolidozor', ],
+    license     = 'Lesser General Public License v3',
+    #download_url = 'https://github.com/bolidozor/RMDS-data-uploader',
+
+    test_suite = 'tests',
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Natural Language :: Czech',
+        # 'Operating System :: OS Independent',
+        # 'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ]
+)
+
---
+++ src/rmds_data_uploader/__init__.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""rmds_data_uploader module.
+
+Author: Jan Milík <milikjan@fit.cvut.cz>
+"""
+
+
+__version__ = "0.1~dev"
+
+
+import sys
+import time
+
+import daemon
+import lockfile
+
+import mlabutils.app
+
+
+class UploaderApp(mlabutils.app.CLIAppBase):
+	def setup_app(self):
+		mlabutils.app.CLIAppBase.setup_app(self)
+		self.app_name = __name__
+
+		self.pidfile_name = "/var/run/%s.pid" % (self.app_name, )
+		self.daemon_context = daemon.DaemonContext(
+			pidfile = lockfile.FileLock(self.pidfile_name)
+		)
+
+	def run(self):
+		if self.daemon_context.pidfile.is_locked():
+			sys.stderr.write("PID file %s is locked. Daemon is probably running.\n" % (
+				self.pidfile_name,
+			))
+			return 1
+		else:
+			sys.stderr.write("PID file is not locked.\n")
+
+		print "Starting daemon..."
+		with self.daemon_context:
+			print "In daemon!"
+			self.run_daemon()
+
+	def run_daemon(self):
+		while True:
+			try:
+				with open("/home/jan/rmds_data_uploader.log", "a") as f:
+					f.write(time.ctime() + "\n")
+			except Exception:
+				pass
+			time.sleep(3)
+
+
+def main():
+	app = UploaderApp()
+	app.main()
+
+
+if __name__ == "__main__":
+	main()
+
