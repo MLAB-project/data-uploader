d53fb61a09f5e4615dcb690050fdca25cd3c8a67
Jan Milik <milikjan@fit.cvut.cz>
2014-11-30 18:08:49 +0100

    Update setup.py to add entry script. Add kill_bzupld script. Inherit from mlabutils.app.DaemonAppBase.

--- src/rmds_data_uploader/app.py
+++ src/rmds_data_uploader/app.py
@@ -15,29 +15,10 @@ import lockfile
 import mlabutils.app
 
 
-class UploaderApp(mlabutils.app.CLIAppBase):
+class UploaderApp(mlabutils.app.DaemonAppBase):
 	def setup_app(self):
-		mlabutils.app.CLIAppBase.setup_app(self)
 		self.app_name = __name__
-
-		self.pidfile_name = "/var/run/%s.pid" % (self.app_name, )
-		self.daemon_context = daemon.DaemonContext(
-			pidfile = lockfile.FileLock(self.pidfile_name)
-		)
-
-	def run(self):
-		if self.daemon_context.pidfile.is_locked():
-			sys.stderr.write("PID file %s is locked. Daemon is probably running.\n" % (
-				self.pidfile_name,
-			))
-			return 1
-		else:
-			sys.stderr.write("PID file is not locked.\n")
-
-		print "Starting daemon..."
-		with self.daemon_context:
-			print "In daemon!"
-			self.run_daemon()
+		mlabutils.app.DaemonAppBase.setup_app(self)
 
 	def run_daemon(self):
 		while True:
