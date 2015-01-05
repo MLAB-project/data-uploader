63498d416a2a88183062441a98b7fcf8a4a67cd8
Jakub Kakona <kaklik@mlab.cz>
2015-01-05 23:32:49 +0100

    rsync parameters modified to delete source files from station as soon as are successfully transferred to the data server.

--- config.py
+++ config.py
@@ -1,24 +1,21 @@
 import array
 
-# Name of station (which appears in data file names)
-Station = "OBSUPICE-R1"
-# Project session name
-StationSpace = "OBSUPICE-R1"
-# Project session name (Server data folder for given username)
-UserSpace = "OBSUPICE"
-# user name which will be used for login to server. 
-UserName = "zebrak"
-# path to station output files
-path = "/media/nfs/OBSUPICE/OBSUPICE-R1/"
-# folder with raw data records ("audio/" or "meteors")
+# Znacka pajmenovavani souboru
+Station = "uFlu-R1"
+# Znacka pozorovani
+StationSpace = "uFlu-R1"
+# jmeno stanice
+UserSpace = "uFlu"
+UserName = "uFlu"
+# Cesta k neserazenym datum
+path = "/home/odroid/Bolidozor/uFlu/uFlu-R1/"
+# slozka s raw meteory ("audio/","meteors")
 path_audio = "meteors/"
-# folder with waterfall snapshots "capture/" (SpectrumLab) or "snapshots/"(Radio-observer)
+# slozka s obrazky ("capture/","snapshots/")
 path_image = "snapshots/"
-# folder with station metadata ("data/","data/")
+# slozka s daty ("data/","data/")
 path_data = "data/"
-# path to folder for sorting and local data storage (eg. network NAS)
-path_sort = "/media/nfs/OBSUPICE/OBSUPICE-R1/Sort/"
-# how much space should by cleared with 1GB free space treshold [GB] (does not work for now)
-datavolume = 2279
-# Output data version eg: "Bolidozor_14", "RadObs_14_7"
+# misto k rozrazeni
+path_sort = "/home/odroid/Bolidozor/uFlu/uFlu-R1/Sort/"
+# Verze vstupnich dat eg: "Bolidozor_14", "RadObs_14_7"
 Version = "RadObs_14_7"
