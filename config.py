156216cd3fa28ca8b53fbd03c231f250059658ef
kakl <kakl@astro.cz>
2015-01-20 11:55:26 +0100

    Default configuration was changed.

--- config.py
+++ config.py
@@ -1,21 +1,20 @@
 import array
 
-# Znacka pajmenovavani souboru
-Station = "uFlu-R1"
-# Znacka pozorovani
-StationSpace = "uFlu-R1"
-# jmeno stanice
-UserSpace = "uFlu"
-UserName = "uFlu"
-# Cesta k neserazenym datum
-path = "/home/odroid/Bolidozor/uFlu/uFlu-R1/"
-# slozka s raw meteory ("audio/","meteors")
+# Station name
+Station = "TEST-R0"
+StationSpace = Station
+# Observatory name and space.astro.cz logon name
+UserSpace = "svakov"
+UserName = UserSpace
+# Path to unsorted data
+path = "/home/odroid/Bolidozor/TEST-R0/"
+# Subdirectory with RAW meteors records ("audio/","meteors")
 path_audio = "meteors/"
-# slozka s obrazky ("capture/","snapshots/")
+# Subdirectory with snapshots ("capture/","snapshots/")
 path_image = "snapshots/"
-# slozka s daty ("data/","data/")
+# Subdirectory with metadata ("data/","data/")
 path_data = "data/"
-# misto k rozrazeni
-path_sort = "/home/odroid/Bolidozor/uFlu/uFlu-R1/Sort/"
-# Verze vstupnich dat eg: "Bolidozor_14", "RadObs_14_7"
+# Space for sorted data
+path_sort = "/home/odroid/Bolidozor/TEST-R0/Sort/"
+# Version of data format
 Version = "RadObs_14_7"
